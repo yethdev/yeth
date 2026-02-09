2af4956dbc68b34b5ce8edfe27499cdacadbfcbf
Susan Miller <owner@Susans-MacBook-Air.local>
2026-02-08 18:56:24 -0600

    Updates

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE = 'yeth21'
+
+self.addEventListener('install', () => self.skipWaiting())
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', (e) => {
+  const { request } = e
+  const url = new URL(request.url)
+
+  if (request.method !== 'GET' || url.origin !== self.location.origin) return
+
+  if (request.mode === 'navigate') {
+    e.respondWith(
+      fetch(request).then((res) => {
+        caches.open(CACHE).then((c) => c.put(request, res.clone()))
+        return res
+      }).catch(() => caches.match('/index.html'))
+    )
+    return
+  }
+
+  if (url.pathname.startsWith('/assets/')) {
+    e.respondWith(
+      caches.match(request).then((cached) =>
+        cached || fetch(request).then((res) => {
+          caches.open(CACHE).then((c) => c.put(request, res.clone()))
+          return res
+        })
+      )
+    )
+    return
+  }
+
+  e.respondWith(
+    caches.match(request).then((cached) => {
+      const net = fetch(request).then((res) => {
+        caches.open(CACHE).then((c) => c.put(request, res.clone()))
+        return res
+      })
+      return cached || net
+    })
+  )
+})
